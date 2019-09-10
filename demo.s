918bdd8927d594ab683a5426687469281180a019
Visa-Valtteri Pimiä <vp79799@gmail.com>
2019-09-10 18:04:28 +0300

    heilutus

--- demo.s
+++ demo.s
@@ -155,6 +155,7 @@ yclr2:
 
 mainloop:
 
+
 			lda changesong
 			cmp #1
 			bne nochangesong
@@ -413,32 +414,8 @@ dologo2:
 	cmp #2
 	beq nologofade
 
-	ldy #200
-yloop:
-	lda #11
-	clc
-    sbc partframes2
-    ror
-    ror
-    ror
-    sta x1
 
-	lda $ff1e
-	and #7
-	and x1
-	sta x2
-	lda #0
-	clc
-	adc x2
-	sta $ff07
 
-logowiper:
-
-donelogoy:
-
-	dey
-	cpy #0
-	bne yloop
 
 nologofade:
 
@@ -886,41 +863,72 @@ rastercnt: .byte 0
 lumavals: .byte 32,64,128,82,14,55,191
 
 irq_vector: ;;; start of frame
-	pha
-	txa
-	pha
-	tya
-	pha
+	;; count 57 cycles to next scanline
+	pha ;3
+	txa ;2
+	pha ;3
+	tya ;2
+	pha ;3
 
-	asl $ff09
+	asl $ff09 ;6
+
+	;19
 
 	lda demopart
-	cmp #3
-	bne gotoplayer
+	cmp #0
+	bne gotoplayer2
+
+	lda $ff1d
+	adc frame
+	adc frame
+	tax
+	lda sintab,x
+	ror 
+	ror 
+	ror 
+	ror 
+	ror 
+	and #7
+	clc
+	adc #%00001000 
+	sta $ff07       
+
+	clc
+	lda $ff1d
+	cmp #$c9
+	bcc do_screen_irq
+gotoplayer2:
+	lda demopart ;4
+	cmp #3 ;2
+	bne gotoplayer ;2+1
 
 	lda runinit
 	cmp #2
 	bne gotoplayer
 
-	nop
-	nop
-	nop
-	nop
+	nop 
+	nop 
+	nop 
+	nop 
 
-	lda #$f1
-	sta $ff19 ;; border
+	lda #$f1 ;2
+	sta $ff19 ;; border 4
 now:
 	clc
 	lda $ff1d
 	cmp #$c9
 	bcc do_screen_irq
 gotoplayer:
+
     lda #<irq_vector2    ; Set IRQ vector to be called
     sta $FFFE           ; Once per screen refresh
     lda #>irq_vector2
     sta $FFFF           ; The $ 314 / $ 315 vector points to the IRQ raster routine
 
-    jmp screen_irq_done
+    lda demopart
+    cmp #0
+    bne screen_irq_done
+    beq screen_irq_done2
 
 do_screen_irq:
 
@@ -935,6 +943,14 @@ screen_irq_done:
     inc rastercnt
     inc rastercnt
     inc rastercnt
+
+    jmp ohihihi
+
+screen_irq_done2:
+    inc rastercnt
+    inc rastercnt
+
+ohihihi:
 
     lda #%00000010  ;; use interrupt source: raster counter
     sta $ff0a
@@ -1405,8 +1421,21 @@ runexit:
 	jmp mainloop
 
 
-
-
+sintab:
+.byte 128, 131, 134, 137, 140, 143, 146, 149, 152, 155, 158, 162, 165, 167, 170, 173
+.byte 176, 179, 182, 185, 188, 190, 193, 196, 198, 201, 203, 206, 208, 211, 213, 21
+.byte 218, 220, 222, 224, 226, 228, 230, 232, 234, 235, 237, 238, 240, 241, 243, 244
+.byte 245, 246, 248, 249, 250, 250, 251, 252, 253, 253, 254, 254, 254, 255, 255, 255
+.byte 255, 255, 255, 255, 254, 254, 254, 253, 253, 252, 251, 250, 250, 249, 248, 246
+.byte 245, 244, 243, 241, 240, 238, 237, 235, 234, 232, 230, 228, 226, 224, 222, 220
+.byte 218, 215, 213, 211, 208, 206, 203, 201, 198, 196, 193, 190, 188, 185, 182, 179
+.byte 176, 173, 170, 167, 165, 162, 158, 155, 152, 149, 146, 143, 140, 137, 134, 131
+.byte 128, 124, 121, 118, 115, 112, 109, 106, 103, 100, 97, 93, 90, 88, 85, 82, 79
+.byte 76, 73, 70, 67, 65, 62, 59, 57, 54, 52, 49, 47, 44, 42, 40, 37, 35, 33, 31, 29
+.byte 27, 25, 23, 21, 20, 18, 17, 15, 14, 12, 11, 10, 9, 7, 6, 5, 5, 4, 3, 2, 2, 1, 1
+.byte 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 3, 4, 5, 5, 6, 7, 9, 10, 11, 12, 14, 15, 17
+.byte 18, 20, 21, 23, 25, 27, 29, 31, 33, 35, 37, 40, 42, 44, 47, 49, 52, 54, 57, 59, 62
+.byte 65, 67, 70, 73, 76, 79, 82, 85, 88, 90, 93, 97, 100, 103, 106, 109, 112, 115, 118, 121, 124
 
 .res $4000 - *
 
